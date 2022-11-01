2fdbde5c4ef062f3ece4267cb358691f572e6bc0
name <koffimawuli.adjoda@gmail.com>
2022-11-01 21:57:10 +0100

    add mapstruct mapper

--- spring-boot-crud/src/main/java/com/esprit/springbootcrud/repository/PharmacyRepository.java
+++ spring-boot-crud/src/main/java/com/esprit/springbootcrud/repository/PharmacyRepository.java
@@ -2,6 +2,8 @@ package com.esprit.springbootcrud.repository;
 
 import com.esprit.springbootcrud.entity.Pharmacy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
 }
